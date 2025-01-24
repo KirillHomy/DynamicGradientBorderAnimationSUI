1e842d9ecb27e18ecb0dfb18cea2215d43790b20
Kirill Khomicevich <83289514+KirillHomy@users.noreply.github.com>
2025-01-24 17:53:15 +0200

    Initial Commit

---
+++ DynamicGradientBorderAnimationSUI/DynamicGradientBorderAnimationSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicGradientBorderAnimationSUIApp.swift
+//  DynamicGradientBorderAnimationSUI
+//
+//  Created by Kirill Khomicevich on 24.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DynamicGradientBorderAnimationSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
