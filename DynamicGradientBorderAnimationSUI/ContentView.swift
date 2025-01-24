34d1473fb60c21a84bd3cbe6691b68f476c20bb6
Kirill Khomicevich <83289514+KirillHomy@users.noreply.github.com>
2025-01-24 18:15:44 +0200

    Finish

--- DynamicGradientBorderAnimationSUI/ContentView.swift
+++ DynamicGradientBorderAnimationSUI/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var rotationAngle: CGFloat = .zero
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray
+            // Main view
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(width: 260, height: 440)
+                .foregroundStyle(Color.black)
+                .shadow(color: .purple, radius: 10, x: 0, y: 5)
+            
+            // Border
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .frame(width: 130, height: 500)
+                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [Color.yellow,
+                                                                            Color.blue]),
+                                                startPoint: .top,
+                                                endPoint: .bottom))
+                .rotationEffect(.degrees(rotationAngle))
+            // Mask for border
+                .mask {
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
+                        .stroke(lineWidth: 5)
+                        .frame(width: 257, height: 437)
+                }
+            Text("CAROUSEL")
+                .bold()
+                .font(.largeTitle)
+                .foregroundStyle(.white)
         }
-        .padding()
+        .ignoresSafeArea()
+        .onAppear {
+            withAnimation(.linear(duration: 4).repeatForever(autoreverses: false)) {
+                rotationAngle = 360
+            }
+        }
     }
 }
 
